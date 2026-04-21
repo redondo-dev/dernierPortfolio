78b2c89d280990ac74a786a473fedb9096497389
redondo <riad.reda.fethi@gmail.com>
2026-04-21 13:32:25 +0200

    ajout projet cinephoria cda context

--- lib/data.ts
+++ lib/data.ts
@@ -1,76 +1,107 @@
-import { Project } from './types';
+import { Project } from "./types";
 
 export const projects: Project[] = [
   {
     title: "Best Of Foot",
-    description: "Application web pour les meilleurs footballeurs de l'histoire ",
+    description:
+      "Application web pour les meilleurs footballeurs de l'histoire ",
     tech: ["React", "TypeScript", "Next.js", "Tailwind CSS"],
-    image: "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1747561589/bestoffoot.png",
+    image:
+      "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1747561589/bestoffoot.png",
     github: "https://github.com/redondo-dev/best10",
-    demo: "https://bestl0-app.vercel.app"
+    demo: "https://bestl0-app.vercel.app",
   },
   {
     title: "Cabinet Medical",
-    description: "Application pour un cabinet médical d'esthetique avec epilation laser ",
+    description:
+      "Application pour un cabinet médical d'esthetique avec epilation laser ",
     tech: ["React,Nextjs ,Tailwind css "],
-    image: "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1747561627/cabinetMedical_qr3qky.jpg",
+    image:
+      "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1747561627/cabinetMedical_qr3qky.jpg",
     github: "https://github.com/redondo-dev/cabinetMedical",
-    demo: "https://cabinet-medical-gray.vercel.app"
+    demo: "https://cabinet-medical-gray.vercel.app",
   },
   {
     title: "Caferiad",
     description: "Site vitrine pour un marque de café Marseillais",
     tech: ["React", "Next.js", "Tailwind CSS"],
-    image: "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1747563072/caferiad_s0ny2x.png",
+    image:
+      "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1747563072/caferiad_s0ny2x.png",
     github: "https://github.com/redondo-dev/riad-cofee",
-    demo: "https://riad-cofee.vercel.app"
+    demo: "https://riad-cofee.vercel.app",
   },
   {
     title: "Weather App",
-    description: "Application météo avec géolocalisation et prévisions sur 5 jours",
-    tech: ["React-native ","Expo", "OpenWeather API" ],
-    image: "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1747561642/meteoApp_yjtiq3.png",
+    description:
+      "Application météo avec géolocalisation et prévisions sur 5 jours",
+    tech: ["React-native ", "Expo", "OpenWeather API"],
+    image:
+      "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1747561642/meteoApp_yjtiq3.png",
     github: "https://github.com/redondo-dev/MeteoApp-",
-    demo: "https://weather-app-redondo-dev.vercel.app/"
+    demo: "https://weather-app-redondo-dev.vercel.app/",
   },
   {
     title: "AMVTC13",
     description: "Site pour un chauffeur de voiture vtc Marseillais ",
     tech: ["React"],
-    image: "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1736706464/toyota-car_fz08h8.jpg",
+    image:
+      "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1736706464/toyota-car_fz08h8.jpg",
     github: "https://github.com/redondo-dev/mariaVTC",
-    demo: "https://amvtc13-d0249.firebaseapp.com/"
+    demo: "https://amvtc13-d0249.firebaseapp.com/",
   },
   {
     title: "panier E-commerce",
     description: "Panier avec react bootstrap et une fake api e-commerce",
     tech: ["React"],
-    image: "https://cdn.pixabay.com/photo/2019/09/30/15/23/online-shopping-4516043_640.jpg",
+    image:
+      "https://cdn.pixabay.com/photo/2019/09/30/15/23/online-shopping-4516043_640.jpg",
     github: "https://github.com/redondo-dev/cart-e-commerce",
-    demo: "https://panier-e-commerce-en-react.firebaseapp.com"
+    demo: "https://panier-e-commerce-en-react.firebaseapp.com",
   },
   {
     title: "Portfolio Anouar",
     description: "Portfolio pour un etudiant en science po ",
     tech: ["Next-js"],
-    image: "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1747563068/anouarPortfolio_xah9zn.png",
+    image:
+      "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1747563068/anouarPortfolio_xah9zn.png",
     github: "https://github.com/redondo-dev/AnourPortfolio",
-    demo: "https://portfolio-anouar.vercel.app/"
+    demo: "https://portfolio-anouar.vercel.app/",
   },
   {
     title: "AMVTC refait en Next js",
     description: "Site pour un chauffeur vtc Marseillais ",
     tech: ["Next-js"],
-    image: "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1747563067/amvtcnext_czeyxx.png",
+    image:
+      "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1747563067/amvtcnext_czeyxx.png",
     github: "privé",
-    demo: "https://www.marseilletaxivtc.com/"
+    demo: "https://www.marseilletaxivtc.com/",
   },
   {
     title: "Site cinema redaflix :en cours de developpement",
-    description: "Recuperation d'une api cinema IMDB et ajout de register,login,logout,reservations ",
-    tech: ["Angular19",'Nodejs','Express','Sql', 'docker', 'fly.io'],
-    image: "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1749030865/redaflix_is7wsi.png",
+    description:
+      "Recuperation d'une api cinema IMDB et ajout de register,login,logout,reservations ",
+    tech: ["Angular19", "Nodejs", "Express", "Sql", "docker", "fly.io"],
+    image:
+      "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1749030865/redaflix_is7wsi.png",
     github: "https://github.com/redondo-dev/cinematek_Angular",
-    demo: "https://redaflix-frontend.fly.dev/"
-  }
+    demo: "https://redaflix-frontend.fly.dev/",
+  },
+  {
+    title: "Cinephoria : projet CDA",
+    description:
+      "Creation d'une api cinema en Node Js Express et frontend en Angular 19 ,utilisation de Docker et CI/CD avec deployement sur Render ",
+    tech: [
+      "Angular19",
+      "Nodejs",
+      "Express",
+      "Sql",
+      "Nosql",
+      "Docker",
+      "Render",
+    ],
+    image:
+      "https://res.cloudinary.com/dzyq5ko7q/image/upload/v1776771055/Capture_d_%C3%A9cran_Cinephoria_ou5c7v.png",
+    github: "https://github.com/redondo-dev/cinephoria-frontend",
+    demo: "https://frontend-eight-xi-87.vercel.app",
+  },
 ];
